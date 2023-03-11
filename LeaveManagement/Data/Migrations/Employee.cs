651dda0bc009c2c967d1483e9eaaab438bb667dc
anandharajmurugaperumal@gmail.com <anandharajmurugaperumal@gmail.com>
2023-03-11 23:00:23 +0530

    Adding Basic tables

---
+++ LeaveManagement/Data/Migrations/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Data.Migrations
+{
+    public class Employee : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? TaxtID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfJoined { get; set; }
+
+    }
+}
